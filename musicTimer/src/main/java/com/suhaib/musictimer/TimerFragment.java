d941e63e77ecb6e2bc3149d486ae36f8303eeefc
Suhaib Khan <suheb.work@gmail.com>
2016-10-31 18:53:21 +0530

    Updating admob sdk and adding interstitial ads
    Signed-off-by: Suhaib Khan <suheb.work@gmail.com>

--- musicTimer/src/main/java/com/suhaib/musictimer/TimerFragment.java
+++ musicTimer/src/main/java/com/suhaib/musictimer/TimerFragment.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -15,94 +16,131 @@ import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 import com.haloappstudio.musictimer.R;
 import com.suhaib.musictimer.utils.Utils;
 
 public class TimerFragment extends Fragment {
 
-	private BroadcastReceiver mTimerRecevier;
-	private IntentFilter mIntentFilter;
-	private TextView mTimerText, mStopText;
-	private ProgressBar mTimerBar;
-	private Fragment mFragment;
+    private BroadcastReceiver mTimerRecevier;
+    private IntentFilter mIntentFilter;
+    private TextView mTimerText, mStopText;
+    private ProgressBar mTimerBar;
+    private Fragment mFragment;
+    private InterstitialAd mInterstitialAd;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-		View rootView = inflater.inflate(R.layout.fragment_timer, container,
-				false);
-		mFragment = new StartFragment();
-		mStopText = (TextView) rootView.findViewById(R.id.stopText);
-		mTimerText = (TextView) rootView.findViewById(R.id.timerText);
+        View rootView = inflater.inflate(R.layout.fragment_timer, container,
+                false);
+        mFragment = new StartFragment();
+        mStopText = (TextView) rootView.findViewById(R.id.stopText);
+        mTimerText = (TextView) rootView.findViewById(R.id.timerText);
 
-		// Custom font
-		Typeface font = Typeface.createFromAsset(getActivity().getAssets(),
-				"fonts/Roboto-Light.ttf");
-		mStopText.setTypeface(font);
-		mTimerText.setTypeface(font);
-		
-		mTimerBar = (ProgressBar) rootView.findViewById(R.id.timerBar);
-		mTimerBar.setOnTouchListener(new View.OnTouchListener() {
-			@Override
-			public boolean onTouch(View view, MotionEvent event) {
-				if (event.getAction() == MotionEvent.ACTION_DOWN) {
-					mTimerText.setTextColor(getResources().getColor(
-							R.color.bloodred));
-					mStopText.setTextColor(getResources().getColor(
-							R.color.bloodred));
-				} else {
-					mTimerText.setTextColor(Color.BLACK);
-					mStopText.setTextColor(Color.BLACK);
-				}
-				return false;
-			}
-		});
-		mTimerBar.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Intent serviceIntent = new Intent(view.getContext(),
-						TimerService.class);
-				if(getActivity().stopService(serviceIntent) == true) {
-					TimerService.mServiceFlag = false;
-				}
-				Utils.changeFragment(mFragment, getActivity());
-			}
-		});
-		mTimerRecevier = new BroadcastReceiver() {
+        // Create interstitial ad
+        mInterstitialAd = new InterstitialAd(getActivity());
+        mInterstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
 
-			@Override
-			public void onReceive(Context context, Intent intent) {
-				int currentTime = (int) intent.getLongExtra("currentTime", 0);
-				int maxTime = (int) intent.getLongExtra("maxTime", 0);
-				String formattedTime = intent.getStringExtra("formattedTime");
-				mTimerText.setText(formattedTime);
-				mTimerBar.setMax(maxTime);
-				mTimerBar.setProgress(currentTime);
-				if (intent.getBooleanExtra("timer_flag", false)
-						|| !TimerService.mServiceFlag) {
-					Utils.changeFragment(mFragment, getActivity());
-				}
-			}
-		};
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+                stopTimerService();
+            }
+        });
 
-		return rootView;
-	}
+        requestNewInterstitial();
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		if(!TimerService.mServiceFlag) {
-			Utils.changeFragment(mFragment, getActivity());
-		}
-		mIntentFilter = new IntentFilter(Utils.ACTION_UPDATE_TIMER);
-		getActivity().registerReceiver(mTimerRecevier, mIntentFilter);
-	}
+        // Custom font
+        Typeface font = Typeface.createFromAsset(getActivity().getAssets(),
+                "fonts/Roboto-Light.ttf");
+        mStopText.setTypeface(font);
+        mTimerText.setTypeface(font);
 
-	@Override
-	public void onPause() {
-		super.onPause();
-		getActivity().unregisterReceiver(mTimerRecevier);
-	}
+        mTimerBar = (ProgressBar) rootView.findViewById(R.id.timerBar);
+        mTimerBar.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    mTimerText.setTextColor(getResources().getColor(
+                            R.color.bloodred));
+                    mStopText.setTextColor(getResources().getColor(
+                            R.color.bloodred));
+                } else {
+                    mTimerText.setTextColor(Color.BLACK);
+                    mStopText.setTextColor(Color.BLACK);
+                }
+                return false;
+            }
+        });
+        mTimerBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                    Log.d("MAIN", "INTERSTITIAL AD SHOWN");
+                } else {
+                    Log.d("MAIN", "INTERSTITIAL AD NOT SHOWN");
+                    stopTimerService();
+                }
+            }
+        });
+        mTimerRecevier = new BroadcastReceiver() {
 
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int currentTime = (int) intent.getLongExtra("currentTime", 0);
+                int maxTime = (int) intent.getLongExtra("maxTime", 0);
+                String formattedTime = intent.getStringExtra("formattedTime");
+                mTimerText.setText(formattedTime);
+                mTimerBar.setMax(maxTime);
+                mTimerBar.setProgress(currentTime);
+                if (intent.getBooleanExtra("timer_flag", false)
+                        || !TimerService.mServiceFlag) {
+                    Utils.changeFragment(mFragment, getActivity());
+                }
+            }
+        };
+
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!TimerService.mServiceFlag) {
+            Utils.changeFragment(mFragment, getActivity());
+        }
+        mIntentFilter = new IntentFilter(Utils.ACTION_UPDATE_TIMER);
+        getActivity().registerReceiver(mTimerRecevier, mIntentFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getActivity().unregisterReceiver(mTimerRecevier);
+    }
+
+    private void requestNewInterstitial() {
+        if (!mInterstitialAd.isLoading() && !mInterstitialAd.isLoaded()) {
+            AdRequest adRequest = new AdRequest.Builder()
+                    .addTestDevice(getString(R.string.oneplus3_device_id))
+                    .build();
+            mInterstitialAd.loadAd(adRequest);
+            Log.d("MAIN", "LOAD INTERSTITIAL AD");
+        }
+    }
+
+    private void stopTimerService() {
+        Intent serviceIntent = new Intent(getActivity(),
+                TimerService.class);
+        if (getActivity().stopService(serviceIntent) == true) {
+            TimerService.mServiceFlag = false;
+        }
+        Utils.changeFragment(mFragment, getActivity());
+    }
 }
